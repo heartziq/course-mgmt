8c991c08f05864b28f09d29f5b92adf179485ade
heartziq <heartziq@gmail.com>
2021-05-30 23:26:52 +0800

    incorporated Material-UI; More UI/UX to come

--- cmgmt-react/src/index.js
+++ cmgmt-react/src/index.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import '@fontsource/roboto';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,16 +9,31 @@ import {
 } from "react-router-dom";
 
 import { CookiesProvider, useCookies } from 'react-cookie';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 
+// Load components
 import CourseDetails from './components/CourseDetails';
 import AllCourses from './components/AllCourses';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './components/Dashboard';
+import DashboardC from './components/DashBoardCompo';
 
 // Page not found
 import PageNotFound from './components/NotFound';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import { Delete as DeleteIcon, DeleteForever as DeleteForeverIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
 
 
 export default function Root() {
@@ -30,20 +46,33 @@ export default function Root() {
 const authContext = createContext();
 
 function Draft() {
+  let classes = useStyles();
   let auth = useAuth()
   useEffect(() =>
     fetch("/draft")
       .then(res => res.json())
       .then(({ token }) => {
-        
+
         auth.signout(token)
       }), [])
   return (
-    <div>
-      <h1>coo</h1>
+    <div className={classes.root}>
+      <Typography variant="h1" component="h2" gutterBottom>
+        h1. Heading
+      </Typography>
       <Link to="/Dashboard/4">
         to dashboard
       </Link>
+
+      <Grid container className={classes.root}>
+        <Grid item xs={4}>
+          <Typography>Filled</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <DeleteIcon color="primary" style={{ fontSize: 65 }} />
+          <DeleteForeverIcon fontSize="large" style={{ color: green[500] }} />
+        </Grid>
+      </Grid>
     </div>
 
   )
@@ -62,7 +91,7 @@ function useProvideAuth() {
     callback()
   };
 
-  const signout = (token="") => {
+  const signout = (token = "") => {
     let newTokenValue = token || null
     setToken(newTokenValue)
   };
@@ -92,6 +121,7 @@ function App() {
   console.log("cookies: ", cookies.name)
   return (
     <ProvideAuth>
+
       <Router>
         <div>
           <nav>
@@ -104,6 +134,9 @@ function App() {
               </li>
               <li>
                 <Link to="/draft">Get cookie</Link>
+              </li>
+              <li>
+                <Link to="/dashC">DashboardC</Link>
               </li>
             </ul>
           </nav>
@@ -125,6 +158,9 @@ function App() {
             </Route>
             <Route path="/draft">
               <Draft />
+            </Route>
+            <Route path="/dashC">
+              <DashboardC />
             </Route>
             <Route component={PageNotFound} />
           </Switch>
